f89e0210a68c193709d2fce8f719bf8c86a84ab9
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-01-10 16:41:20 +0600

    all done

---
+++ src/helpers/hitToast.js
@@ -0,0 +1,37 @@
+import { toast } from 'react-toastify';
+
+const hitToast = (variant) => {
+    // if (!['success', 'error'].includes(variant)) throw Error();
+    // toast[`${variant}`](message, {
+    //     position: "top-right",
+    //     autoClose: 3000,
+    //     hideProgressBar: false,
+    //     closeOnClick: true,
+    //     pauseOnHover: true,
+    //     draggable: true,
+    //     progress: undefined,
+    // });
+    if(variant.acknowledged){
+        toast.success('Successfully added!', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
+    else {
+        toast.error('Already subscribe!', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
+}
+export default hitToast;
